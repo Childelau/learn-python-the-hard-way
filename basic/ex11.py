1e27671e69ef07e5c688bdc13b54c29ea4cac985
Childelau <1299337429@qq.com>
2017-10-13 17:05:19 +0800

    add existed files

---
+++ basic/ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = int(raw_input())
+print "How tall are you?",
+height = int(raw_input())
+print "How much do you weight",
+weight = int(raw_input())
+
+
+print "So, you're %r old, %r tall, %r heavy." % (age, height, weight)
